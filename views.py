7607de82ff8fac0ae452bf0197a1641394b4ddf6
Nedim444 <57811298+Nedim444@users.noreply.github.com>
2021-08-20 16:34:57 +0200

    Update views.py

--- views.py
+++ views.py
@@ -2,27 +2,26 @@ from Finance.models import User,Transaction
 from Finance.forms import TransactionForm, UserForm
 from django.shortcuts import render,redirect
 from django.forms.formsets import formset_factory
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def add_transaction(request):
-    TransactionFormSet = formset_factory(TransactionForm, extra = 10, min_num=1, validate_min = True)
     if(request.method == 'POST'):
         user_form = UserForm(request.POST)
-        transaction_formset = TransactionForm(request.POST)
-        if all([user_form.is_valid() , transaction_formset.is_valid()]):
+        transaction_form = TransactionForm(request.POST)
+        if (user_form.is_valid() and transaction_formset.is_valid()):
             user = user_form.save()
-            for inline_form in transaction_formset:
-                if inline_form.cleaned_data:
-                    transaction = inline_form.save(commit=False)
-                    transaction.user_id = user
-                    transaction.save()
-            return render(request,'Finance/index.html', {})
+            transaction_form.cleaned_data['user_id'] = user
+            transaction = transaction_form.save()
+            return HttpResponseRedirect(request,'Finance/index.html', {})
         else:
-            user_form = UserForm()
-            transaction_formset = TransactionForm()
-        return render(request,'Finance/add_transaction.html', {
+            print('Failed')
+      else:
+        user_form = UserForm()
+        transaction_formset = TransactionForm()
+    return render(request,'Finance/add_transaction.html', {
             'user_form':user_form,
-            'transaction_form': transaction_formset,
+            'transaction_form': transaction_form,
         })
 
 def add_users(request):
